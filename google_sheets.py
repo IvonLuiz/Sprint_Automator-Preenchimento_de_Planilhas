8dcc20cc69e60b64f3d2d7ef32d95f780f96677c
IvonLuiz <ivonluizgarcia@gmail.com>
2023-12-18 19:27:48 -0300

    Novas funcionalidades, input funcionando e parte do output

--- google_sheets.py
+++ google_sheets.py
@@ -1,14 +1,58 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-def authenticate_gsheets(credentials_path):
-    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name(credentials_path, scope)
-    client = gspread.authorize(creds)
-    return client
+# def authenticate_gsheets(credentials_path):
+#     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+#     creds = ServiceAccountCredentials.from_json_keyfile_name(credentials_path, scope)
+#     client = gspread.authorize(creds)
+#     return client
 
-def append_to_spreadsheet(client, spreadsheet_url, data, worksheet_index=0):
-    spreadsheet = client.open_by_url(spreadsheet_url)
-    worksheet = spreadsheet.get_worksheet(worksheet_index)
-    rows_to_append = data.to_dict('records')
-    worksheet.append_rows(rows_to_append, value_input_option='USER_ENTERED')
+# def append_to_spreadsheet(client, spreadsheet_url, data, worksheet_index=0):
+#     spreadsheet = client.open_by_url(spreadsheet_url)
+#     worksheet = spreadsheet.get_worksheet(worksheet_index)
+#     rows_to_append = data.to_dict('records')
+#     worksheet.append_rows(rows_to_append, value_input_option='USER_ENTERED')
+
+def clear_and_append_worksheet(worksheet, clear_matrix=['A2:ZZ300'], data_to_append=0):
+        worksheet.batch_clear(clear_matrix)
+        worksheet.append_rows(data_to_append)
+
+
+def find_column_index(worksheet, header_name):
+        df = pd.DataFrame(worksheet.get_all_records())
+        return df.columns.get_loc(header_name) + 1
+
+
+def apply_formula_to_column(worksheet, formula, column, start_row, end_row):
+        # Apply the formula to each cell in the specified range
+        for row in range(start_row, end_row + 1):
+                formula_string = formula.format(column, row)
+                cell_range = f"{column}{row}"
+                worksheet.update(cell_range, formula_string, raw=False)
+
+
+# Updated function to apply the formula to a specific column and range
+def apply_formula_to_column_name(worksheet, formula, column, start_row, end_row):
+        # Find the column index based on the header name
+        column_index = find_column_index(worksheet, column)
+        print(column_index)
+        # Apply the formula to each cell in the specified range
+        for row in range(start_row, end_row + 1):
+                formula_string = formula.format(column_index, row)
+                cell_range = f"{chr(65 + column_index - 1)}{row}"  # Convert column index to letter
+                worksheet.update(cell_range, formula_string, raw=False)
+
+
+def find_related(dataframe, search_column, target_word, return_column):
+        contains_word = dataframe[search_column].str.contains(target_word, case=False)
+
+        # Get the rows where the target_word is present
+        rows_with_word = dataframe[contains_word]
+
+        if not rows_with_word.empty:
+                # Get the values from the specified return_column where the target_word is present
+                values_from_return_column = rows_with_word[return_column]
+
+                return values_from_return_column
+        else:
+                return pd.DataFrame(), None
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import pandas as pd
-# from openpyxl import Workbook, load_workbook
 import gspread
-# from google_sheets import authenticate_gsheets, append_to_spreadsheet
+from google_sheets import *
 # worksheet.format('A1:B1', {'textFormat': {'bold': True}})
 
 # Variables
 path_to_file = 'sheets//tasks.xls'
-sprint_number = 39
+sprint_number = 35
 
 tasks_file = pd.read_html(path_to_file)[0]  # Use [0] to get the first DataFrame returned by read_html()
 
@@ -30,41 +29,66 @@ wks = database.worksheet('Tarefas')
 data = pd.DataFrame(wks.get_all_records())
 print(data)
 
-def find_related(dataframe, search_column, target_word, return_column):
-        contains_word = dataframe[search_column].str.contains(target_word, case=False)
-
-        # Get the rows where the target_word is present
-        rows_with_word = dataframe[contains_word]
-
-        if not rows_with_word.empty:
-                # Get the values from the specified return_column where the target_word is present
-                values_from_return_column = rows_with_word[return_column]
-
-                return values_from_return_column
-        else:
-                return pd.DataFrame(), None
-
-
 print(find_related(data, 'Responsavel', 'Adriana Chaves', 'Função'))
 
 # Input_Bitrix worksheet
+
 input_wks = database.worksheet('Input_Bitrix')
-data = pd.DataFrame(input_wks.get_all_records())
-print(data)
 
-# Clear cells except header
-worksheet_range = input_wks.range("A2:ZZ1000")  
+# Clear cells except header (Adjust if necessary)
+clear_and_append_worksheet(input_wks, ['A2:ZZ300'], rows_from_sprint.fillna('').values.tolist()[1:])
 
-for cell in worksheet_range:
-        cell.value = ""
-
-input_wks.update_cells(worksheet_range)
-input_wks.update(rows_from_sprint.fillna('').values.tolist()[1:])
-
-formula = '=indirect("Sheet1!B2")<>"hello"'
 
 # Output_Bitrix worksheet
+
+
+
 output_wks = database.worksheet('Output_Bitrix')
+
+# Update the worksheet with the formula in column A from row 2 to the end of rows_from_sprint
+apply_formula_to_column(worksheet = output_wks, 
+                        formula = "=EXT.TEXTO(Input_Bitrix!B{1};23;2)",
+                        column = 'A', 
+                        start_row = 2, 
+                        end_row = len(rows_from_sprint))
+
+apply_formula_to_column(worksheet = output_wks, 
+                        formula = "=Input_Bitrix!A{1}",
+                        column = 'B', 
+                        start_row = 2, 
+                        end_row = len(rows_from_sprint))
+
+apply_formula_to_column_name(worksheet = output_wks, 
+                        formula = "=EXT.TEXTO(Input_Bitrix!B{1};27;5000)",
+                        column = 'Task', 
+                        start_row = 2, 
+                        end_row = len(rows_from_sprint))
+
+apply_formula_to_column_name(worksheet = output_wks, 
+                        formula = "=Input_Bitrix!G{1}",
+                        column = 'Responsavel', 
+                        start_row = 2, 
+                        end_row = len(rows_from_sprint))
+
+
+# size = len(rows_from_sprint)
+# # Assuming 'Sprint' is a column header in your worksheet
+# sprint_column_header = 'Sprint'
+# sprint_values = [sprint_number] * len(rows_from_sprint)
+
+# # Find the column index corresponding to the 'Sprint' header
+# sprint_column_index = output_wks.columns.get_loc(sprint_column_header) + 1  # Adding 1 to convert from 0-based to 1-based index
+
+# # Update the entire column with the sprint value
+# input_wks.update_cell(1, sprint_column_index, sprint_values)
+
+# # Add formula to column A for each row (starting from the second row)
+# formula_range = output_wks.range('A2:A' + str(len(data_to_append) + 1))
+# formula = '=EXT.TEXTO(Input_Bitrix!B2;23;2)'  # Your specified formula
+# for cell in formula_range:
+#         cell.value = formula
+
+# output_wks.update_cells(formula_range)
 
 
 
